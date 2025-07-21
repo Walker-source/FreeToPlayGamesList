d145556c3734f942939becff32d1ecf026fdbb18
MaDoG <aliasgraphics@icloud.com>
2025-07-21 18:51:07 +0300

    Initial Commit

---
+++ FreeToPlayGamesList/FreeToPlayGamesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeToPlayGamesListApp.swift
+//  FreeToPlayGamesList
+//
+//  Created by Denis Lachikhin on 21.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct FreeToPlayGamesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
