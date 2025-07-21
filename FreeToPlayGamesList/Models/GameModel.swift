8fa2278475c3eb39d2144310dbc03eba7c318a9e
MaDoG <aliasgraphics@icloud.com>
2025-07-21 19:49:14 +0300

    GameModel

---
+++ FreeToPlayGamesList/Models/GameModel.swift
@@ -0,0 +1,32 @@
+//
+//  GameModel.swift
+//  FreeToPlayGamesList
+//
+//  Created by Denis Lachikhin on 21.07.2025.
+//
+
+import Foundation
+
+struct Game: Decodable {
+    let id: Int
+    var title: String
+    let thumbnail: URL
+    let shortDescription: String
+    let gameUrl: URL
+    let genre: String
+    let platform: String
+    let publisher: String
+    let developer: String
+    let releaseDate: String
+    let freetogameProfileUrl: URL
+    
+    var about: String {
+        """
+        Genre: \(genre).
+        Platform: \(platform).
+        Publisher: \(publisher).
+        Developer: \(developer).
+        Relseas date: \(releaseDate).
+        """
+    }
+}
