1d8e73e8ed1d70be4587945e6b19c26253d8e165
MaDoG <aliasgraphics@icloud.com>
2025-07-22 19:45:13 +0300

    Thumbnail properties

---
+++ FreeToPlayGamesList/Utilities/AppCustomization.swift
@@ -0,0 +1,16 @@
+//
+//  AppCustomization.swift
+//  FreeToPlayGamesList
+//
+//  Created by Denis Lachikhin on 22.07.2025.
+//
+
+import Foundation
+
+// MARK: - Thumbnail Properties
+final class ThumbnailCustomization {
+    static let thumbnailWith: CGFloat = 150
+    static let thumbnailHeight: CGFloat = 100
+    static let thumbnailCornerRadius: CGFloat = 15
+    static let thumbnailShadowRadius: CGFloat = 3
+}
