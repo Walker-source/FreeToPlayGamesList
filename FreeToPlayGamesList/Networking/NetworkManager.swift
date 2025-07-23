48b8cb6ab2f969778abc7fe2b5ecda1359f8058a
MaDoG <aliasgraphics@icloud.com>
2025-07-23 15:58:11 +0300

    Refactoring

--- FreeToPlayGamesList/Networking/NetworkManager.swift
+++ FreeToPlayGamesList/Networking/NetworkManager.swift
@@ -65,13 +65,4 @@ final class NetworkManager {
                 
             }.resume()
     }
-    func fetchData<T: Decodable> (from url: URL, type: T.Type) async throws -> T {
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse,
-              200..<300 ~= httpResponse.statusCode else {
-            throw URLError(.badServerResponse)
-        }
-        return try JSONDecoder().decode(T.self, from: data)
-    }
 }
