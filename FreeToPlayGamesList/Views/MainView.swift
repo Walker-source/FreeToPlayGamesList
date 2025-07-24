b382c22771af0ed981ae7646303febd8b19e0155
MaDoG <aliasgraphics@icloud.com>
2025-07-24 11:01:43 +0300

    MainView with tabBar

---
+++ FreeToPlayGamesList/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  FreeToPlayGamesList
+//
+//  Created by Denis Lachikhin on 24.07.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
