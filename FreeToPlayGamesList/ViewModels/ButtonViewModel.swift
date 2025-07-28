490614161ee9fc48a58c2b673250d64fd7b648f4
MaDoG <aliasgraphics@icloud.com>
2025-07-28 14:19:02 +0300

    Button view model

---
+++ FreeToPlayGamesList/ViewModels/ButtonViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonViewModel.swift
+//  FreeToPlayGamesList
+//
+//  Created by Denis Lachikhin on 28.07.2025.
+//
+
+import SwiftUI
+
+struct ButtonViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonViewModel()
+}
