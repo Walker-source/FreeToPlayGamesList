488482706c08de18ff7359d8f93ac401d2deb518
MaDoG <aliasgraphics@icloud.com>
2025-07-28 16:06:59 +0300

    Link button castomization

--- FreeToPlayGamesList/Utilities/AppCustomization.swift
+++ FreeToPlayGamesList/Utilities/AppCustomization.swift
@@ -15,13 +15,26 @@ final class ThumbnailCustomization {
     static let thumbnailShadowRadius: CGFloat = 3
 }
 
+// MARK: - Game View Properties
 final class GameViewCustomization {
     static let vstackPadding: CGFloat = 15
+    
+    // MARK: - Thumbnail Properies
     static let thumbnailImageHeight: CGFloat = 200
     static let thumbnailCornerRaduius: CGFloat = 15
     static let thumbnailShadowRaduius: CGFloat = 5
+    
+    // MARK: - Description Text Properties
     static let descriptionTextSize: CGFloat = 20
     static let descriptionTextFrameWidth: CGFloat = 340
+    
+    // MARK: - Label Properties
     static let aboutLabelTextSize: CGFloat = 15
     static let aboutLabelOpacity: CGFloat = 0.7
+}
+
+// MARK: - Game View Button Properties
+final class GameViewButtonCustomization {
+    static let frameWidth: CGFloat = 150
+    static let frameHeight: CGFloat = 50
 }
