ef7fc9f60a97d0ded64256eadbded5979fb2f3e1
MaDoG <aliasgraphics@icloud.com>
2025-08-14 16:09:10 +0300

    Refactoring

--- FreeToPlayGamesList/Networking/AsyncImageCached.swift
+++ FreeToPlayGamesList/Networking/AsyncImageCached.swift
@@ -42,67 +42,15 @@ struct AsyncImageCached<Content>: View where Content: View {
         }
     }
     
-    private func load(url: URL) async {
-        let nsUrl = url as NSURL
-        let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 60)
-        
-        // Memory Cache
-        if let chachedImage = ImageMemoryCache.shared.object(forKey: nsUrl) {
-            self.loadedImage = Image(uiImage: chachedImage)
-            return
-        }
-        
-        // Cache Check
-//        if let cached = URLCache.shared.cachedResponse(for: request),
-//           let uiImage = UIImage(data: cached.data) {
-//            withAnimation(transaction.animation) {
-//                self.loadedImage = Image(uiImage: uiImage)
-//            }
-//            return
-//        }
-        
-        // Disk Cache
-        if let cached = URLCache.shared.cachedResponse(for: request),
-           let uiImage = UIImage(data: cached.data) {
-            ImageMemoryCache.shared.setObject(uiImage, forKey: nsUrl)
-            self.loadedImage = Image(uiImage: uiImage)
-            return
-        }
-        
-        // Network Load
-        do {
-            let (data, response) = try await URLSession.shared.data(for: request)
-            if let uiImage = UIImage(data: data) {
-                // Save to memory
-                ImageMemoryCache.shared.setObject(uiImage, forKey: nsUrl)
-                
-                // Save to disk
-                let cachedResponse = CachedURLResponse(response: response, data: data)
-                URLCache.shared.storeCachedResponse(cachedResponse, for: request)
-                
-                //Show image
-                self.loadedImage = Image(uiImage: uiImage)
-//                withAnimation(transaction.animation) {
-//                    self.loadedImage = Image(uiImage: uiImage)
-//                }
-            }
-        } catch {
-            print("Load error", error)
-        }
-    }
     private func loadImage(url: URL) {
         networkManager.fetchImage(from: url) { result in
             switch result {
             case .success(let uiImage):
                 let image = UIImage(data: uiImage)
                 loadedImage = Image(uiImage: image ?? UIImage())
-            case .failure(let failure):
+            case .failure(_):
                 return
             }
         }
     }
 }
-
-//#Preview {
-//    AsyncImageCached()
-//}
